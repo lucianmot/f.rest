03e1e196199daa864c86938871ee06f36c56e49f
Aleksandra <ajmpawlik@gmail.com>
2019-08-08 12:25:31 +0100

    REFACTOR: creates Tokeniser class

--- tokeniser_test.py
+++ tokeniser_test.py
@@ -1,23 +1,23 @@
 import unittest
 
-class TestInterpreter(unittest.TestCase):
+class TestTokeniser(unittest.TestCase):
 
     def test_method_returns_string_token_when_passed_e(self):
-        from forest import Interpreter
-        interpreter = Interpreter("echo<<e>>")
-        self.assertEqual(interpreter.tokeniser(), [{"ECHO" : "echo"}, {"STRSTART" : "<<"}, {"STRING" : "e"}, {"STRSTOP" : ">>"}])
+        from forest import Tokeniser
+        tokeniser = Tokeniser("echo<<e>>")
+        self.assertEqual(tokeniser.create_token(), [{"ECHO" : "echo"}, {"STRSTART" : "<<"}, {"STRING" : "e"}, {"STRSTOP" : ">>"}])
 
     def test_method_returns_string_token_when_passed_a(self):
-        from forest import Interpreter
-        interpreter = Interpreter("echo<<a>>")
-        self.assertEqual(interpreter.tokeniser(), [{"ECHO" : "echo"}, {"STRSTART" : "<<"}, {"STRING" : "a"}, {"STRSTOP" : ">>"}])
+        from forest import Tokeniser
+        tokeniser = Tokeniser("echo<<a>>")
+        self.assertEqual(tokeniser.create_token(), [{"ECHO" : "echo"}, {"STRSTART" : "<<"}, {"STRING" : "a"}, {"STRSTOP" : ">>"}])
 
     def test_method_returns_string_token_when_passed_caps_z(self):
-        from forest import Interpreter
-        interpreter = Interpreter("echo<<Z>>")
-        self.assertEqual(interpreter.tokeniser(), [{"ECHO" : "echo"}, {"STRSTART" : "<<"}, {"STRING" : "Z"}, {"STRSTOP" : ">>"}])
+        from forest import Tokeniser
+        tokeniser = Tokeniser("echo<<Z>>")
+        self.assertEqual(tokeniser.create_token(), [{"ECHO" : "echo"}, {"STRSTART" : "<<"}, {"STRING" : "Z"}, {"STRSTOP" : ">>"}])
 
     def test_method_returns_exception_when_passed_number(self):
-        from forest import Interpreter
-        interpreter = Interpreter("9")
-        self.assertRaises(Exception, interpreter.tokeniser)
+        from forest import Tokeniser
+        tokeniser = Tokeniser("9")
+        self.assertRaises(Exception, tokeniser.create_token)
