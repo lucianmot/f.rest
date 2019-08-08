2546ea72a9ff2e54bb8a84d409eb266a168389d0
joseph townshend <joetownshend@mac.com>
2019-08-08 12:34:50 +0100

    REFACTOR: response method to use tokeniser class

--- forest.py
+++ forest.py
@@ -6,28 +6,9 @@ class Interpreter(object):
         self.text = text
 
     def response(self):
-        token = self.tokeniser()[2]
+        tokeniser = Tokeniser(self.text)
+        token = tokeniser.create_token()[2]
         return token.get("STRING")
-
-    # def tokeniser(self):
-    #     if bool(re.match("echo", self.text)) == True:
-    #         echo = self.text[0:4]
-    #         restofstr = self.text[4:]
-    #         if bool(re.match("<<", restofstr)) == False:
-    #             raise Exception("Error 1")
-    #         elif bool(re.match("<<", restofstr)) == True:
-    #             if bool(re.search(">>", restofstr)) == False:
-    #                 raise Exception("Error 2")
-    #             elif bool(re.search(">>", restofstr)) == True:
-    #                 strstart = restofstr[0:2]
-    #                 middle = restofstr[2:-2]
-    #                 strstop = restofstr[-2:]
-    #                 return [{"ECHO": echo},
-    #                         {"STRSTART" : strstart},
-    #                         {"STRING" : middle},
-    #                         {"STRSTOP" : strstop}]
-    #     else:
-    #         raise Exception("Error 3")
 
 
 class Tokeniser(object):
