43c7522cf8156953d2e309b9860edcf33d4b673d
James Hylins <james.hylins@johnlewis.co.uk>
2019-08-09 10:52:41 +0100

    admin: remove all comments from test files

--- interpreter_test.py
+++ interpreter_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 class TestInterpreter(unittest.TestCase):
-#    @unittest.skip("test_interpreter_should_return_A_when_user_enters_A")
+
     def test_interpreter_should_return_e_when_user_enters_e(self):
         from forest import Interpreter
         interpreter = Interpreter("echo<<e>>")
@@ -15,31 +15,9 @@ class TestInterpreter(unittest.TestCase):
     def test_interpreter_should_return_Exception_when_user_enters_4(self):
         from forest import Interpreter
         interpreter = Interpreter("echo<<4>>")
-    #    self.assertRaises(Exception, interpreter.response)
         self.assertEqual(interpreter.response(), "4")
 
     def test_interpreter_should_return_Hello_World(self):
         from forest import Interpreter
         interpreter = Interpreter("echo<<Hello World!>>")
-    #    self.assertRaises(Exception, interpreter.response)
         self.assertEqual(interpreter.response(), "Hello World!")
-
-    # def test_interpreter_should_return_nothing_when_echo_with_empty_string(self):
-    #     from forest import Interpreter
-    #     interpreter = Interpreter("echo <<>>")
-    #     self.assertEqual(interpreter.response(), "")
-
-    # def test_interpreter_should_return_a_when_echo_with_a(self):
-    #     from forest import Interpreter
-    #     interpreter = Interpreter("echo <<a>>")
-    #     self.assertEqual(interpreter.response(), "a")
-
-    # def test_interpreter_should_return_hello_when_echo_with_hello(self):
-    #     from forest import Interpreter
-    #     interpreter = Interpreter("echo <<hello>>")
-    #     self.assertEqual(interpreter.response(), "hello")
-
-    # def test_interpreter_should_return_hello_world_when_echo_with_hello_world(self):
-    #     from forest import Interpreter
-    #     interpreter = Interpreter("echo <<Hello, world!>>")
-    #     self.assertEqual(interpreter.response(), "Hello, world!")
--- parser_test.py
+++ parser_test.py
@@ -1,15 +1,7 @@
 import unittest
 
-#The parser accepts token array
-#Checks token keys against grammar rules
-#Returns true is it's a valid sentence ??????
-#Returns false if it not.
-
 class TestParser(unittest.TestCase):
     def test_something(self):
-        #Given that the parser gets one token (ECHO)
-        #When parser is run
-        #It returns true
         from forest import Parser
         tokens = {"ECHO": 1, "STRSTART" : 2, "STRING" : 3, "STRSTOP" : 4}
         parser = Parser(tokens)
