3ac38838337a42eec00a78b4c81a236b51971bf9
James Hylins <james.hylins@johnlewis.co.uk>
2019-08-09 11:01:19 +0100

    refactor: DRY out test files putting import statement at top of files rather than in tests

--- interpreter_test.py
+++ interpreter_test.py
@@ -1,23 +1,20 @@
 import unittest
+from forest import Interpreter
 
 class TestInterpreter(unittest.TestCase):
 
     def test_interpreter_should_return_e_when_user_enters_e(self):
-        from forest import Interpreter
         interpreter = Interpreter("echo<<e>>")
         self.assertEqual(interpreter.response(), "e")
 
     def test_interpreter_should_return_A_when_user_enters_A(self):
-        from forest import Interpreter
         interpreter = Interpreter("echo<<A>>")
         self.assertEqual(interpreter.response(), "A")
 
     def test_interpreter_should_return_Exception_when_user_enters_4(self):
-        from forest import Interpreter
         interpreter = Interpreter("echo<<4>>")
         self.assertEqual(interpreter.response(), "4")
 
     def test_interpreter_should_return_Hello_World(self):
-        from forest import Interpreter
         interpreter = Interpreter("echo<<Hello World!>>")
         self.assertEqual(interpreter.response(), "Hello World!")
--- parser_test.py
+++ parser_test.py
@@ -1,8 +1,8 @@
 import unittest
+from forest import Parser
 
 class TestParser(unittest.TestCase):
     def test_something(self):
-        from forest import Parser
         tokens = {"ECHO": 1, "STRSTART" : 2, "STRING" : 3, "STRSTOP" : 4}
         parser = Parser(tokens)
         self.assertEqual(parser.run_parser(), True)
