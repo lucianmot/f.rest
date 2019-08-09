4f060ed183f0791b65a3deb2614e0ade08a25e3b
joseph townshend <joetownshend@mac.com>
2019-08-09 11:38:29 +0100

    TESTS: add test runner

--- interpreter_test.py
+++ interpreter_test.py
@@ -50,3 +50,6 @@ class TestParser(unittest.TestCase):
         tokens = {"ECHO": 1, "STRSTART" : 2, "STRING" : 3, "STRSTOP" : 4}
         parser = Parser(tokens)
         self.assertEqual(parser.run_parser(), True)
+        
+if __name__ == '__main__':
+    unittest.main()
