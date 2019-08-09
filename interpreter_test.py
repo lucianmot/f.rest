250d28ec93aa4e9c00f507e79cb54222454bacfa
joseph townshend <joetownshend@mac.com>
2019-08-09 11:32:34 +0100

    TESTS: moved all tests to one file

--- interpreter_test.py
+++ interpreter_test.py
@@ -22,3 +22,31 @@ class TestInterpreter(unittest.TestCase):
     def test_interpreter_should_return_Hello_World(self):
         interpreter = Interpreter("echo<<Hello World!>>")
         self.assertEqual(interpreter.response(), "Hello World!")
+
+from forest import Tokeniser
+
+class TestTokeniser(unittest.TestCase):
+
+    def test_method_returns_string_token_when_passed_e(self):
+        tokeniser = Tokeniser("echo<<e>>")
+        self.assertEqual(tokeniser.create_token(), {"ECHO" : "echo", "STRSTART" : "<<", "STRING" : "e", "STRSTOP" : ">>"})
+
+    def test_method_returns_string_token_when_passed_a(self):
+        tokeniser = Tokeniser("echo<<a>>")
+        self.assertEqual(tokeniser.create_token(), {"ECHO" : "echo", "STRSTART" : "<<", "STRING" : "a", "STRSTOP" : ">>"})
+
+    def test_method_returns_string_token_when_passed_caps_z(self):
+        tokeniser = Tokeniser("echo<<Z>>")
+        self.assertEqual(tokeniser.create_token(), {"ECHO" : "echo", "STRSTART" : "<<", "STRING" : "Z", "STRSTOP" : ">>"})
+
+    def test_method_returns_exception_when_passed_number(self):
+        tokeniser = Tokeniser("9")
+        self.assertRaises(Exception, tokeniser.create_token)
+
+from forest import Parser
+
+class TestParser(unittest.TestCase):
+    def test_something(self):
+        tokens = {"ECHO": 1, "STRSTART" : 2, "STRING" : 3, "STRSTOP" : 4}
+        parser = Parser(tokens)
+        self.assertEqual(parser.run_parser(), True)
